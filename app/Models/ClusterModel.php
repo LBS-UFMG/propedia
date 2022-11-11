19f15822a4831dc9c775f115445d04f99faf909a
Diego Mariano <diogohenks@hotmail.com>
2022-11-11 16:34:13 -0300

    Update ClusterModel.php

--- app/Models/ClusterModel.php
+++ app/Models/ClusterModel.php
@@ -197,20 +197,20 @@ class ClusterModel extends Model {
 
 		$db->select("count(cluster_num) number_singletons_sequence");
 		$db->where("cluster_num", -1);
-		$number_singletons_sequence = $db->get()->getRow()->number_singletons_sequence;
+		$number_singletons_sequence = $db->get()->getResult()[0]->number_singletons_sequence;
 
 		$db->select("count(distinct cluster_num) as number_cluster_interface");
 		$db->from("cluster_interface");
-		$number_cluster_interface = $db->get()->getRow()->number_cluster_interface;
+		$number_cluster_interface = $db->get()->getResult()[0]->number_cluster_interface;
 
 		$db->select("count(cluster_num) number_singletons_interface");
 		$db->from("cluster_interface");
 		$db->where("cluster_num", -1);
-		$number_singletons_interface = $db->get()->getRow()->number_singletons_interface;
+		$number_singletons_interface = $db->get()->getResult()[0]->number_singletons_interface;
 
 		$db->select("count(distinct cluster_num) as number_cluster_binding");
 		$db->from("cluster_binding");
-		$number_cluster_binding = $db->get()->getRow()->number_cluster_binding;
+		$number_cluster_binding = $db->get()->getResult()[0]->number_cluster_binding;
 
 		$db->select("count(cluster_num) number_singletons_binding");
 		$db->from("cluster_binding");
