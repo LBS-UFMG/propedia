1f50e4e0d721d7f3a8f74d7522f0cc02c6f6d319
Diego Mariano <diogohenks@hotmail.com>
2022-11-14 17:00:50 -0300

    Update footer.php

--- web/application/views/template/footer.php
+++ web/application/views/template/footer.php
@@ -162,10 +162,12 @@
    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
          <div class="modal-header">
-            <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <div>
                <h3><b>Download</b></h3>
             </div>
+
+            <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
          </div>
          <div class="modal-body">
             <div class="row">
@@ -200,15 +202,15 @@
          </div>
          <div class="modal-footer">
             <div class="row">
-               <div class="col-md-8 col-sm-12">
-                  <p id="feedback_download" class="text-left" style="display: block;"></p>
+               <div class="col-12">
+                  <p id="feedback_download" class="text-end" style="display: block;"></p>
                   <div id="progress_bar_div" class="progress" style="display: block;">
                      <div id="progress_bar_value" class="progress-bar progress-bar-success progress-bar-striped active" aria-valuenow="0" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:0%">
                         0%
                      </div>
                   </div>
                </div>
-               <div class="col-md-4 col-sm-12">
+               <div class="col-12">
                   <div class="row">
                      <div class="col-md-12 col-sm-12">
                         <button id="btn_download_modal" type="button" class="btn btn-success btn-block">Download</button>
