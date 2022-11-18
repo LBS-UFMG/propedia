0d7f340e6665a453bf194e703095e126a585ad18
Diego Mariano <diogohenks@hotmail.com>
2022-11-18 15:25:03 -0300

    Update explore.php

--- web/application/views/explore.php
+++ web/application/views/explore.php
@@ -242,6 +242,9 @@
 </div>
 
 <script>
-   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+   $(()=>{
+      const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+      const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+   });
 </script>
